87c64a209b4fa983f267ee039ab28171b3b66af4
TheWill55 <127157385+TheWill55@users.noreply.github.com>
2023-04-24 20:32:21 -0300

    js atualização

---
+++ Javascript/ex02/script2.js
@@ -0,0 +1,9 @@
+let num1, num2 
+num1= prompt("Digite o primeiro numero.")
+num2 = prompt ("Digite o segundo numero.")
+
+let resultado
+
+resultado = Number (num1) - Number (num2)
+
+alert(resultado)
